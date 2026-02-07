5623d37217e4f3c5ac01c33c30ea553383d5dc08
“Ivan <miltonivan117@gmail.com>
2026-02-06 21:53:23 -0600

    chore: update Astro site configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://env-shelf.dev",
-  integrations: [tailwind()]
+  site: "https://env-shelf.vercel.app",
+  integrations: [tailwind()],
 });
